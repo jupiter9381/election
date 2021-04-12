094b9ad53bc6340e618c20ec03744b781e47bc83
Jameslnew <75798098+Jameslnew@users.noreply.github.com>
2021-04-13 01:36:02 +1000

    candidate detail page

---
+++ dashboard/candidate.php
@@ -0,0 +1,101 @@
+<?php 
+	session_start();
+	include "../includes/databse_con.php";
+	if(!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
+		$base_path = "";
+		header("Location: ../default.php");
+	}
+	else{
+		$base_path = "../";
+	}
+	if($_GET && isset($_GET['id'])) {
+		$sql = "SELECT * FROM kandidat WHERE id='".$_GET['id']."'";
+    	$result = mysqli_query($conn, $sql);
+    	if (mysqli_num_rows($result) > 0) {
+    		$candidate = mysqli_fetch_assoc($result);
+    	} else {
+    		header("Location: ./dashboard");
+    	}
+	} else {
+		header("Location: ./dashboard");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../assets/css/jquery-confirm.min.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+  <nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+      <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">VALG</label>
+    <ul>
+      <li><a href="<?php echo $base_path;?>dashboard/" class="active">Home</a></li>
+      <li><a href="<?php echo $base_path;?>nominering.php">Nominering</a></li>
+      <li><a href="<?php echo $base_path;?>includes/loggut.php">logg ut</a></li>
+    </ul> 
+  </nav>
+  <section class="main-content">
+    <section class="content-grid">
+    	<section>
+	        <section class="grid-border fix-content-grid align-center mb-10">
+	      		<section class="rounded-circle img-thumbnail avatar" style="background-image:url(../assets/images/profile/<?php echo $_SESSION['img_url'];?>);" title="profile image"></section>
+	      		<section>
+	      			<section class="profile-name">
+	              <span><?php echo $_SESSION['fname'] . ' ' . $_SESSION['lname'];?></span>
+	            </section>
+	            <section class="profile-role">(<?php echo $_SESSION['brukertype']; ?>)</section>
+	            <section class="profile-text"><?php echo $_SESSION['email']; ?></section>
+	      		</section>
+	        </section>
+	        <a href="update-profile.php">
+	          <button tyle="button" class="btn btn-primary btn-sm btn-block login-button mb-10"><i class="far fa-edit"></i> Update Profile</button>
+	        </a>
+	        <a href="update-usertype.php">
+	          <button tyle="button" class="btn btn-primary btn-sm btn-block login-button mb-10"><i class="far fa-edit"></i> Update Role</button>
+	        </a>
+	    	</section>
+	    	<section>
+	        <section class="text-right mb-10">
+	          <button class="btn btn-danger" id="velg" onclick="setElection()"><i class="fas fa-users"></i> Velg</button>
+	          <button class="btn btn-danger" id="withdraw" onclick="withdrawCandidacy()" style="display: none"><i class="fas fa-user-slash"></i> Withdraw Candidacy</button>
+	          <button class="btn btn-success" id="nominate" onclick="nominateMe()" style="display: none"><i class="fas fa-users"></i> Nominer meg selv</button>
+	          <button class="btn btn-primary" onclick="nominateUser()"><i class="fas fa-users"></i> Nominer en bruker</button>
+	          <button class="btn btn-info" onclick="voteUser()"><i class="fas fa-users"></i> Velg en kandidat</button>
+	        </section>
+    		<section class="grid-border">
+    			<h2>Candidate Detail</h2>
+    			<hr class="mtb-10">
+    			<section class="text-center candiate-section">
+    				<section>
+    					<img class="rounded-circle img-profile profile-avatar" src="/assets/images/profile/<?= $candidate['img_url']?>">	
+    				</section>
+    				<section style="max-width: 600px; margin: 0 auto;">
+    					<p><strong>Fakultet:</strong>  <?= $candidate['fakultet']?></p>
+    					<p><strong>Institutt:</strong>  <?= $candidate['institutt']?></p>
+    					<p><strong>Bruker epost:</strong>  <?= $candidate['bruker_epost']?></p>
+    					<p><strong>Kandidat Col:</strong>  <?= $candidate['kandidatcol']?></p>
+    					<p><strong>Bruker</strong>  <?= $candidate['bruker']?></p>
+    					<p><strong>Stemmer</strong>  <?= $candidate['stemmer']?></p>
+    					<p><strong>Informasjon</strong>  <?= $candidate['informasjon']?></p>
+    					<button class="btn btn-success prev_btn" onclick="getCandidate(<?= $candidate['id']?>, 'prev')">Prev</button>
+    					<button class="btn btn-primary next_btn" onclick="getCandidate(<?= $candidate['id']?>, 'next')">Next</button>
+    				</section>
+    			</section>
+    		</section>
+    	</section>
+    </section>
+  </section>
+</body>
+<script type="text/javascript" src="../assets/js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="../assets/js/jquery-confirm.min.js"></script>
+<script type="text/javascript" src="../assets/js/dashboard.js"></script>
+</html>
